5150b9651fe60484d8ec37f8e13ac44f4da75398
Omniprezz <geoffrey.frioli@gmail.com>
2018-06-25 16:21:19 +0200

    test api sur SiteHTML_API

--- SiteHTML_API/main.js
+++ SiteHTML_API/main.js
@@ -1,7 +1,20 @@
-function book() {
-    $.getJSON("https://www.googleapis.com/books/v1/volumes?q=search+terms", function (data) {
-        var affiche = data.items[1].volumeInfo.imageLinks.thumbnail;
-        $("#afficher").html("<img src='" + affiche + "'>");
-        console.log(affiche);
-    });
-};
+
+$.getJSON("https://www.googleapis.com/books/v1/volumes?q=search+terms", function (data) {
+    var affiche = data.items[1].volumeInfo.imageLinks.thumbnail;
+    var titre = data.items[1].volumeInfo.title;
+    var desc = data.items[1].volumeInfo.description;
+    $("#affiche0").html("<img src='" + affiche + "'>");
+    $("#titre0").html(titre);
+    $("#desc0").html(desc);
+    console.log(desc);
+});
+
+$.getJSON("https://www.googleapis.com/books/v1/volumes?q=search+terms", function (data) {
+    var affiche = data.items[2].volumeInfo.imageLinks.thumbnail;
+    var titre = data.items[2].volumeInfo.title;
+    var desc = data.items[2].volumeInfo.description;
+    $("#affiche1").html("<img src='" + affiche + "'>");
+    $("#titre1").html(titre);
+    $("#desc1").html(desc);
+    console.log(desc);
+});
